64e2f3c4fe94944c07132339ccb766760a28dd15
hanjianyucool <hanjianyucool@163.com>
2020-10-25 23:29:00 +0800

    footer code update

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,10 @@
 <footer class="text-center py-4 text-secondary bg-light mt-auto">
   <small>
     <ul class="list-inline mb-0">
-      <li class="list-inline-item">© 慕课网（imooc.com）版权所有 | 津ICP备20000929号-1</li>
-      <li class="list-inline-item"><a href="https://coding.imooc.com/class/449.html" target="_blank">购买课程</a></li>
-      <li class="list-inline-item"><a href="http://docs.vikingship.xyz/" target="_blank">文档</a></li>
-      <li class="list-inline-item"><a href="http://api.vikingship.xyz/" target="_blank">API 在线调试</a></li>
-      <li class="list-inline-item"><a href="http://showcase.vikingship.xyz/" target="_blank">组件库演示</a></li>
+      <li class="list-inline-item">©（个人）版权所有 | 希望有兴趣者共同进步与交流</li>
+      <li class="list-inline-item">微信: 17864307858</li>
+      <li class="list-inline-item">持续更新</li>
+      <li class="list-inline-item">喜欢的点个星星</li>
     </ul>
   </small>
 </footer>
